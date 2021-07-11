fd43dacbcccaeb9db4b0a41f28c4cc6231100ee5
Jason Chu <me@chuie.io>
2021-07-10 22:42:12 -0700

    add macOS support

---
+++ mpv/mpv_unix.go
@@ -0,0 +1,30 @@
+// +build darwin linux
+
+package mpv
+
+import (
+	"net"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func connect() (net.Conn, error) {
+	return net.DialTimeout("unix", "/tmp/ledsimsocket", time.Second)
+}
+
+func runMPV(pathToFile string, debug bool) (*exec.Cmd, error) {
+	cmd := exec.Command("mpv", pathToFile, `--no-video`, `--input-ipc-server=/tmp/ledsimsocket`, `--pause`)
+
+	if debug {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+
+	err := cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	return cmd, nil
+}
