12d1ff160425e54c82d439cdd7ee750c2e6e92f6
Muwahid Ahmed <muwazid@gmail.com>
2022-02-27 12:25:29 +1100

    Hacky workaround for small bug in dissolve
    Before this commit, the last cluster of LEDs would not 'dissolve' so
    this workaround attempts to fix that

--- effects/dissolve.go
+++ effects/dissolve.go
@@ -11,37 +11,35 @@ import (
 )
 
 type Dissolve struct {
-	// ledPeriods []time.Duration
-	// delay      []time.Duration
 	duration  time.Duration
 	closed    []int          // index of which leds have dissolved
 	currClust []int          // index of current cluster of LEDs we want to dissolve
-	reqColor  colorful.Color // required color
-	clustSize int            // How many clusters/groups of LEDs to dissolve at a time
-
-	// period 	   time.Duration
-	// baseline   time.Duration
-	// deviation  time.Duration
-	// EaseFunc func(x float64) float64
+	period    float64        // How long it will take a cluster to dissolve; between 0 and 1 is good
+	reqColor  colorful.Color // required color; Use {0,0,0} for dissolving to black
+	clustSize int            // How many clusters/groups of LEDs to dissolve at a time; between 30 - 80 is good
+	remaining []int          // Index of LEDs that are remaining to be dissolved
+	remCount  int            // Counts number LEDs remaining to be dissolved
 }
 
-func NewDissolve(duration time.Duration, reqColor colorful.Color) *Dissolve {
+func NewDissolve(duration time.Duration, reqColor colorful.Color, clustSize int, period float64) *Dissolve {
 	return &Dissolve{
 		duration:  duration,
-		reqColor:  reqColor,
-		clustSize: 60,
+		reqColor:  reqColor,  // Use {0,0,0} for dissolving to black
+		clustSize: clustSize, // between 30 - 80 is good
+		period:    period,    // between 0 and 1 is good
 	}
 }
 
-// func (d *Dissolve) DFS() {
-
-// }
-
 func (d *Dissolve) OnEnter(sys *ledsim.System) {
 	ledCount := len(sys.LEDs)
-	d.closed = make([]int, ledCount)
+	d.closed = make([]int, 0)
 	d.currClust = make([]int, 0)
-	// d.reqColor = colorful.Color{0.7, 0.3, 0.2}
+	d.remaining = make([]int, 0)
+	d.remCount = ledCount
+
+	for _, led := range sys.LEDs {
+		d.remaining = append(d.remaining, led.ID)
+	}
 
 	// Pick random led and add its neighbours to form a starting cluster
 	for i := 1; i <= d.clustSize; i++ {
@@ -55,14 +53,6 @@ func (d *Dissolve) OnEnter(sys *ledsim.System) {
 }
 
 func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
-	t := progress * float64(d.duration) / float64(time.Second)
-	var period float64 = 0.1
-
-	if t > float64(period) {
-		div := float64(int(t / float64(period)))
-		t = t - (div * float64(period))
-	}
-
 	// Randomly choose an led
 	// Get that LED, and and neighbouring LEDS
 	// That group of LEDs will be a cluster
@@ -72,34 +62,37 @@ func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
 	// Store those LEDS in array closed
 	// Pick another random LED that is not in CLosed
 	// Repeat until all LEDS in closed
+	t := progress * float64(d.duration) / float64(time.Second)
+	var period float64 = d.period
+
+	if t > float64(period) {
+		div := float64(int(t / float64(period)))
+		t = t - (div * float64(period))
+	}
 
 	clustDissolved := false
-	// for _, ledIndx := range d.currClust {
-
-	// 	if sys.LEDs[ledIndx].Color.AlmostEqualRgb(d.reqColor) || (math.Abs(t-period) <= 0.05) {
-	// 		clustDissolved = true
-	// 		fmt.Println("CLUST DISSOLVED")
-	// 		d.closed = append(d.closed, d.currClust...)
-
-	// 		// break
-	// 	}
-
-	// 	sys.LEDs[ledIndx].Color = ledsim.BlendRgb(sys.LEDs[ledIndx].Color, colorful.Color{0.7, 0.3, 0.2}, t/period)
-
-	// }
 
 	for _, led := range sys.LEDs {
-		if contains(d.currClust, led.ID) {
+
+		// If this led is in the current cluster of LEDs we want to dissolve (or if only <100 remaining LEDs to dissolve)
+		if contains(d.currClust, led.ID) || d.remCount < 100 {
+			// If this cluster is 'dissolved'
 			if led.Color.AlmostEqualRgb(d.reqColor) || math.Abs(t-period) < 0.05 {
 				clustDissolved = true
 				// fmt.Println("CLUST DISSOLVED")
 				d.closed = append(d.closed, led.ID)
+				remove(d.remaining, led.ID)
+				if d.remCount > 0 {
+					d.remCount--
+					fmt.Println("remcount is: ", d.remCount)
+				}
 			} else {
 				led.Color = ledsim.BlendRgb(led.Color, d.reqColor, t/period)
 			}
 
 		}
 
+		// If a cluster is already 'dissolved', keep it at the required colour (in most cases {0,0,0})
 		if contains(d.closed, led.ID) {
 			led.Color = d.reqColor
 		}
@@ -107,19 +100,20 @@ func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
 	}
 
 	ledCount := len(sys.LEDs)
+	// If current cluster is dissolved, look for new cluster
 	if clustDissolved {
-		// d.closed = append(d.closed, d.currClust...)
 		d.currClust = nil
 		for i := 1; i <= d.clustSize; i++ {
 			var randLedIndx int
-			if math.Abs(float64(ledCount-len(d.closed))) <= 10 {
-				fmt.Println("Entered")
-				for j := 0; j < ledCount; j++ {
-					fmt.Println("Indx:", j)
-					if !contains(d.closed, j) {
-						randLedIndx = j
-						break
-					}
+
+			if d.remCount == 0 {
+				fmt.Println("remCount is 0")
+				break
+			} else if d.remCount < 100 {
+				randLedIndx = d.remaining[0]
+				remove(d.remaining, randLedIndx)
+				if d.remCount > 0 {
+					d.remCount--
 				}
 			} else {
 				randLedIndx = rand.Intn(ledCount)
@@ -127,6 +121,7 @@ func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
 					randLedIndx = rand.Intn(ledCount)
 				}
 			}
+
 			d.currClust = append(d.currClust, randLedIndx)
 			for _, nbr := range sys.LEDs[randLedIndx].Neighbours {
 				d.currClust = append(d.currClust, nbr.ID)
@@ -144,6 +139,20 @@ func contains(s []int, e int) bool {
 		}
 	}
 	return false
+}
+
+// func remove(s []int, i int) []int {
+// 	s[i] = s[len(s)-1]
+// 	return s[:len(s)-1]
+// }
+
+func remove(l []int, item int) []int {
+	for i, other := range l {
+		if other == item {
+			return append(l[:i], l[i+1:]...)
+		}
+	}
+	return l
 }
 
 func (d *Dissolve) OnExit(sys *ledsim.System) {
