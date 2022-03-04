603cc6fcaf2e80c9909e30203d71305449a30db5
Ricky Qu <34181602+rickqu@users.noreply.github.com>
2022-03-04 21:51:19 +1000

    New segment (#26)
    * Test new segment
    
    * re-enable teensy
    
    * Increase segment speed
    
    * turn on other animations

--- effects/segment.go
+++ effects/segment.go
@@ -1,10 +1,7 @@
 package effects
 
 import (
-	"log"
-	"math"
 	"math/rand"
-	"strconv"
 	"time"
 
 	"ledsim"
@@ -14,33 +11,34 @@ import (
 )
 
 type Segment struct {
-	*colorful.Color
-	FADE_TYPE FADE_TYPE
+	duration  time.Duration
+	baseline  time.Duration
+	deviation time.Duration
+	palette   func() colorful.Color
 
 	initialised bool
-	chainToLeds map[int]*chainProgress
+	chainToLeds map[int]*chain
 	chainOrder  []int
 }
 
-type chainProgress struct {
-	leds     []*ledsim.LED
-	progress float64
+type chain struct {
+	leds   []*ledsim.LED
+	period time.Duration
+	delay  time.Duration
+	colour colorful.Color
 }
 
-func newChainProgress() *chainProgress {
-	return &chainProgress{make([]*ledsim.LED, 0), 0.0}
-}
+func NewSegment(duration, baseline, deviation time.Duration, palette func() colorful.Color) *Segment {
+	return &Segment{
+		duration:  duration,
+		baseline:  baseline,
+		deviation: deviation,
+		palette:   palette,
 
-func (c *chainProgress) isDone() bool {
-	return c.progress >= 1.0
-}
-
-func (c *chainProgress) appendLED(led *ledsim.LED) {
-	c.leds = append(c.leds, led)
-}
-
-func NewSegment(c *colorful.Color, fadeType FADE_TYPE) *Segment {
-	return &Segment{c, fadeType, false, make(map[int]*chainProgress), make([]int, 0)}
+		initialised: false,
+		chainToLeds: make(map[int]*chain),
+		chainOrder:  make([]int, 0),
+	}
 }
 
 func (s *Segment) OnEnter(sys *ledsim.System) {
@@ -48,110 +46,83 @@ func (s *Segment) OnEnter(sys *ledsim.System) {
 		for _, led := range sys.LEDs {
 			_, chainExists := s.chainToLeds[led.Chain]
 			if !chainExists {
-				s.chainToLeds[led.Chain] = newChainProgress()
+				s.chainToLeds[led.Chain] = &chain{make([]*ledsim.LED, 0), 0, 0, colorful.Color{}}
 				s.chainOrder = append(s.chainOrder, led.Chain)
 			}
-			s.chainToLeds[led.Chain].appendLED(led)
+			s.chainToLeds[led.Chain].leds = append(s.chainToLeds[led.Chain].leds, led)
 		}
 		s.initialised = true
 	}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(s.chainOrder), func(i, j int) {
-		s.chainOrder[i], s.chainOrder[j] = s.chainOrder[j], s.chainOrder[i]
-	})
+
+	for _, chainToLed := range s.chainToLeds {
+		delta := time.Duration(rand.Float64() * float64(s.deviation))
+		chainToLed.period = s.baseline + delta - (s.deviation / 2)
+		chainToLed.delay = time.Duration(rand.Float64() * float64(s.duration))
+		chainToLed.colour = s.palette()
+	}
 }
 
 func (s *Segment) Eval(progress float64, sys *ledsim.System) {
-	// if we are fading out, then we need to paint canvas with inital colour.
-	var initialColour colorful.Color
-	if s.FADE_TYPE == FADE_OUT {
-		initialColour = *s.Color
-	}
-	for _, led := range sys.LEDs {
-		led.Color = initialColour
-	}
+	t := time.Duration(progress * float64(s.duration))
 
-	var nthChain int
-	var foundChain bool
-	for i, chain := range s.chainOrder {
-		if !s.chainToLeds[chain].isDone() {
-			nthChain = i
-			foundChain = true
-			break
-		} else {
-			// chain is done, set it to full colour
-			for _, led := range s.chainToLeds[chain].leds {
-				if s.FADE_TYPE == FADE_IN {
-					led.Color = *s.Color
-				} else {
-					led.Color = colorful.Color{0, 0, 0}
-				}
+	// each segment is composed of 4 phases.
+	for _, chain := range s.chainToLeds {
+		leds := chain.leds
+		t := t - chain.delay
+		if t < 0 {
+			continue
+		}
+		// determine our current block number
+		block := t / (chain.period * 4)
+		totalBlocks := (s.duration - chain.delay) / (chain.period * 4)
+		locationInBlock := t % (chain.period * 4)
+		phase := locationInBlock / chain.period
+		locationInPhase := locationInBlock % chain.period
+
+		// do not turn on LEDs that are in their final block.
+		if block >= totalBlocks {
+			continue
+		}
+
+		for _, led := range leds {
+			switch phase {
+			case 0:
+				// black, do nothing
+				break
+			case 1:
+				// fade in
+				led.Color = ledsim.BlendRgb(led.Color, chain.colour, float64(locationInPhase)/float64(chain.period))
+			case 2:
+				// stay on
+				led.Color = chain.colour
+			case 3:
+				// fade out
+				led.Color = ledsim.BlendRgb(led.Color, chain.colour, 1-(float64(locationInPhase)/float64(chain.period)))
 			}
 		}
 	}
-	if !foundChain {
-		log.Println("warn: in segment animation all chains are done but animation is still going")
-		return
-	}
-
-	stepSize := 1.0 / float64(len(s.chainOrder))
-	startMilestone := float64(nthChain) * stepSize
-
-	// If progress factor goes beyond 1 or under 0, then we can get random flashes of colour at the end of a fade.
-	progressFactor := (progress - startMilestone) / stepSize
-	if progressFactor > 1 {
-		progressFactor = 1
-	} else if progressFactor < 0 {
-		progressFactor = 0
-	}
-
-	// fmt.Printf("Progress factor %f\n", progressFactor)
-	var colourIntensity float64
-	if s.FADE_TYPE == FADE_IN {
-		colourIntensity = progressFactor
-	} else {
-		colourIntensity = 1 - progressFactor
-	}
-
-	for _, led := range s.chainToLeds[s.chainOrder[nthChain]].leds {
-		led.Color.R = s.Color.R * colourIntensity
-		led.Color.G = s.Color.G * colourIntensity
-		led.Color.B = s.Color.B * colourIntensity
-	}
-	s.chainToLeds[s.chainOrder[nthChain]].progress = progressFactor
 }
 
 func (s *Segment) OnExit(sys *ledsim.System) {
-	for _, chain := range s.chainToLeds {
-		chain.progress = 0
-	}
+
 }
+
+var _ ledsim.Effect = (*Segment)(nil)
 
 func SegmentGenerator(fadeIn, effect, fadeOut time.Duration, rng *rand.Rand) []*ledsim.Keyframe {
-	totalTime := fadeIn + effect + fadeOut
-	repeats := math.Round(float64(totalTime) / float64(StandardPeriod))
-	playTime := time.Duration(float64(totalTime) / repeats)
-
-	var keyframes []*ledsim.Keyframe
-
-	for i := 0; i < int(repeats); i++ {
-		col := Golds[rng.Intn(len(Golds))]
-
-		keyframes = append(keyframes,
-			&ledsim.Keyframe{
-				Label:    "Segment_MainIn_" + strconv.Itoa(i) + "_" + uuid.New().String(),
-				Offset:   time.Duration(i) * playTime,
-				Duration: playTime / 2,
-				Effect:   NewSegment(&col, FADE_IN),
-			},
-			&ledsim.Keyframe{
-				Label:    "Segment_MainOut_" + strconv.Itoa(i) + "_" + uuid.New().String(),
-				Offset:   time.Duration(i)*playTime + (playTime / 2),
-				Duration: playTime / 2,
-				Effect:   NewSegment(&col, FADE_OUT),
-			},
-		)
+	gold := Golds[rand.Intn(len(Golds))]
+	return []*ledsim.Keyframe{
+		{
+			Label:    "Segment_Main_" + uuid.New().String(),
+			Offset:   0,
+			Duration: fadeIn + fadeOut + effect,
+			Effect: NewSegment(fadeIn+fadeOut+effect, 1*time.Second, 750*time.Millisecond,
+				func() colorful.Color {
+					return gold
+				}),
+			Layer: 1,
+		},
 	}
-
-	return keyframes
 }
+
+// baseline, deviation time.Duration, palette colorful.Color)
