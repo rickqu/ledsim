14e8a43c3966802215746eeb757fdb00cbb54dba
Muwahid Ahmed <muwazid@gmail.com>
2022-03-05 10:24:44 +1100

    Fixed up dissolve effect to prevent flickering and flashing

--- effects/dissolve.go
+++ effects/dissolve.go
@@ -1,6 +1,7 @@
 package effects
 
 import (
+	"fmt"
 	"ledsim"
 	"math"
 	"math/rand"
@@ -18,6 +19,7 @@ type Dissolve struct {
 	clustSize int            // How many clusters/groups of LEDs to dissolve at a time; between 30 - 80 is good
 	remaining []int          // Index of LEDs that are remaining to be dissolved
 	remCount  int            // Counts number LEDs remaining to be dissolved
+	state     []colorful.Color
 }
 
 func NewDissolve(duration time.Duration, reqColor colorful.Color, clustSize int, period float64) *Dissolve {
@@ -35,18 +37,11 @@ func (d *Dissolve) OnEnter(sys *ledsim.System) {
 	d.currClust = make([]int, 0)
 	d.remaining = make([]int, 0)
 	d.remCount = ledCount
+	d.state = make([]colorful.Color, ledCount+1)
 
 	for _, led := range sys.LEDs {
 		d.remaining = append(d.remaining, led.ID)
-	}
-
-	// Pick random led and add its neighbours to form a starting cluster
-	for i := 1; i <= d.clustSize; i++ {
-		randLedIndx := rand.Intn(ledCount)
-		d.currClust = append(d.currClust, randLedIndx)
-		for _, nbr := range sys.LEDs[randLedIndx].Neighbours {
-			d.currClust = append(d.currClust, nbr.ID)
-		}
+		d.state[led.ID] = led.Color
 	}
 
 }
@@ -69,51 +64,54 @@ func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
 		t = t - (div * float64(period))
 	}
 
-	clustDissolved := false
+	clustDissolved := true
+	for _, clustLed := range d.currClust {
+		if !d.state[clustLed].AlmostEqualRgb(d.reqColor) {
+			clustDissolved = false
+		}
+	}
 
+	undissolvedCount := len(sys.LEDs)
 	for _, led := range sys.LEDs {
 
 		// If this led is in the current cluster of LEDs we want to dissolve (or if only <100 remaining LEDs to dissolve)
-		if contains(d.currClust, led.ID) || d.remCount < 100 {
+		if contains(d.currClust, led.ID) /*|| d.remCount < 100*/ {
 			// If this cluster is 'dissolved'
-			if led.Color.AlmostEqualRgb(d.reqColor) || math.Abs(t-period) < 0.05 {
-				clustDissolved = true
-				// fmt.Println("CLUST DISSOLVED")
+			if d.state[led.ID].AlmostEqualRgb(d.reqColor) {
 				d.closed = append(d.closed, led.ID)
 				remove(d.remaining, led.ID)
-				if d.remCount > 0 {
-					d.remCount--
-					// fmt.Println("remcount is: ", d.remCount)
-				}
+
+				led.Color = d.reqColor
+				d.state[led.ID] = d.reqColor
 			} else {
 				led.Color = ledsim.BlendRgb(led.Color, d.reqColor, t/period)
+				d.state[led.ID] = ledsim.BlendRgb(led.Color, d.reqColor, t/period)
 			}
 
+		} else {
+			led.Color = d.state[led.ID]
 		}
 
-		// If a cluster is already 'dissolved', keep it at the required colour (in most cases {0,0,0})
-		if contains(d.closed, led.ID) {
-			led.Color = d.reqColor
+		if d.state[led.ID].AlmostEqualRgb(d.reqColor) {
+			undissolvedCount--
 		}
 
 	}
 
 	ledCount := len(sys.LEDs)
-	// If current cluster is dissolved, look for new cluster
-	if clustDissolved {
+	// Once we have reached limit of period, start looking for new clusters
+	if clustDissolved && math.Abs(t-period) > period-0.1 {
+		fmt.Println("TRUE")
 		d.currClust = nil
 		for i := 1; i <= d.clustSize; i++ {
 			var randLedIndx int
-
-			if d.remCount == 0 {
-				// fmt.Println("remCount is 0")
+			if undissolvedCount == 0 {
+				// fmt.Println("FINISHED")
 				break
-			} else if d.remCount < 100 {
+			} else if undissolvedCount < 100 {
 				randLedIndx = d.remaining[0]
 				remove(d.remaining, randLedIndx)
-				if d.remCount > 0 {
-					d.remCount--
-				}
+
 			} else {
 				randLedIndx = rand.Intn(ledCount)
 				for contains(d.closed, randLedIndx) {
@@ -126,6 +124,8 @@ func (d *Dissolve) Eval(progress float64, sys *ledsim.System) {
 				d.currClust = append(d.currClust, nbr.ID)
 			}
 		}
+		fmt.Println("Curr clust: ", len(d.currClust))
+		fmt.Println("t: ", t)
 
 	}
 
@@ -156,6 +156,12 @@ func remove(l []int, item int) []int {
 
 func (d *Dissolve) OnExit(sys *ledsim.System) {
 
+}
+
+func AlmostEqualRgb(c1 colorful.Color, c2 colorful.Color) bool {
+	return math.Abs(c1.R-c2.R)+
+		math.Abs(c1.G-c2.G)+
+		math.Abs(c1.B-c2.B) < 0.01
 }
 
 var _ ledsim.Effect = (*Dissolve)(nil)
